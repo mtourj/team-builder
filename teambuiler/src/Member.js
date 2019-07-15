f2100ea655d1533a997daf77ab65c425182132fb
Mohammad Tourjoman <mtourjoman01@hotmail.com>
2019-07-15 12:55:10 -0700

    Finished and styled Member component

--- teambuiler/src/Member.js
+++ teambuiler/src/Member.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Member = props => {
   return (
     <div className='member'>
-      
+      <h3>{props.info.name}</h3>
+      <p>{props.info.email}</p>
+      <p>{props.info.role}</p>
     </div>
   );
 };
